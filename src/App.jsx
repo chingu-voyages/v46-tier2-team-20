8734ee1c896c2ef8f426ecc69ed8e9d2fa4f1cc7
AOA19 <aakinyemi19@gmail.com>
2023-10-09 11:10:16 -0400

    Setup project files via Vite with React and Tailwind dependencies

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-orange-500 font-bold underline text-center">
+          Recipe App
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
