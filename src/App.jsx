2b1514da9ba3448b8fb83d86bb9a692d5526ce43
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-18 10:42:56 -0400

    Added fetchData function to App.jsx
    The fetchData function uses axios to fetch data from the Tasty API, given a query string of ingredients

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 // import { useState } from "react";
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  testFun();
-  const testFun = () => {
-    console.log('this is a test');
-  };
+  async function fetchData(ingredientString) {
+    const options = {
+      method: 'GET',
+      url: 'https://tasty.p.rapidapi.com/recipes/list',
+      params: {
+        from: '0',
+        size: '20',
+        q: ingredientString,
+      },
+      headers: {
+        'X-RapidAPI-Key': '76d7fe0a0fmsh0e82783d702bde2p14d53ajsn1fb5e9f06c51',
+        'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchData('green onion eggplant');
 
   return (
     <div>
