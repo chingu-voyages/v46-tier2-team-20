9a73b089933bb2eecae4d4bca23da034582610f7
Emimint <90863470+Emimint@users.noreply.github.com>
2023-10-20 11:36:59 -0400

    add tailwind classes for responsive design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,9 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="header">
-      <h1>What will you make today?</h1>
-      <h2>
+    <div className="sm:px-25 md:px-60 lg:px-60 header text-center h-screen place-items-center">
+      <h1 className="sm:text-3xl md:text-4xl lg:text-5xl">What will you make today?</h1>
+      <h2 className="sm:text-xs md:text-xl lg:text-2xl">
         App name is an app that helps you find and create delicious dishes base on your ingredients!
       </h2>
     </div>
