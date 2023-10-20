35efdf23ab6d3f6cea9646ed5c5a74b39099b93c
Emimint <90863470+Emimint@users.noreply.github.com>
2023-10-20 10:01:09 -0400

    create Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>What will you make today?</h1>
+      <h2>
+        App name is an app that helps you find and create delicious dishes base on your ingredients!
+      </h2>
+    </div>
+  );
+}
+
+export default Header;
