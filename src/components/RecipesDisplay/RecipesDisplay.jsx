ef75ad4c998ce49199aafda1ce2badecf17d99dd
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-21 16:34:26 -0400

    1. use a condition to display data only when data is not null 2. use Array's map method to loop over the recipes data 3. Inside map: destructured each recipe properties 4. return HTML for recipe's img and recipe's name from map method for each recipe data 5. Store the output in a variable called 'data' 6. return data from the RecipesDisplay component

--- src/components/RecipesDisplay/RecipesDisplay.jsx
+++ src/components/RecipesDisplay/RecipesDisplay.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export default function RecipesDisplay({ recipes }) {
-  console.log('recipes: ', recipes);
-  return (
-    <>
-    </>
-  );
+  const data = recipes && recipes.map((recipe) => {
+    const { name, thumbnail_url } = recipe;
+    return (
+      <div>
+        <img src={thumbnail_url} />
+        <h1>{name}</h1>
+      </div>
+    );
+  });
+  return data;
 }
