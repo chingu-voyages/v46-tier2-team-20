4bdeadf1b2a5dc7519446695fa908446923e0eb9
AOA19 <aakinyemi19@gmail.com>
2023-10-24 20:01:27 -0400

    Removed temp search bar from App.jsx and passed props to SearchBar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,33 @@
-// import React from 'react'
+// import { useState } from 'react';
 import './SearchBar.css';
 import { BiSearch } from 'react-icons/bi';
+// import { IoCloseCircleSharp } from 'react-icons/io5';
 
-export default function SearchBar() {
+export default function SearchBar({ handleSearch, setIngredients }) {
+  const handleUserInputChange = (e) => {
+    setIngredients(e.target.value);
+  };
+
+  const handleReset = () => {
+    setIngredients('');
+  };
+
   return (
-    <form className="search-bar relative">
+    <form role="search" className="search-bar relative" onSubmit={handleSearch} onReset={handleReset}>
       <input
         type="text"
-        aria-label="search"
+        name="ingredient"
+        aria-label="Search for ingredients"
         placeholder="eggs tomato cheese..."
         className="border-solid border-2 border-gray-200 rounded-xl py-3 px-5 w-full"
+        onChange={handleUserInputChange}
+        required
       />
-      <button type="button" aria-label="search" className="search-bar__icon absolute inset-y-0 right-[5%]">
+      <button
+        type="submit"
+        aria-label="search"
+        className="search-bar__icon--submit absolute inset-y-0 right-[5%]"
+      >
         <BiSearch className="fill-gray-400 w-5 h-5" />
       </button>
     </form>
