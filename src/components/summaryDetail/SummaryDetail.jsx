a33a27700ed6c89da4adacdf5ec6ba4229fcff6f
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-25 16:24:20 -0400

    Set up SummaryDetail component to accept recipeDetail as props

--- src/components/summaryDetail/SummaryDetail.jsx
+++ src/components/summaryDetail/SummaryDetail.jsx
@@ -1,6 +1,9 @@
-export default function SummaryDetail() {
+export default function SummaryDetail(props) {
   return (
-    <>
-    </>
+    <h1>
+      I want to cook
+      {' '}
+      {props.recipeDetail.name}
+    </h1>
   );
 }
