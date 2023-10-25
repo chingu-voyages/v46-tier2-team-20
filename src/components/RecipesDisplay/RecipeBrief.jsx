0fd7378f6615842fcf73af5a251f7264dc5c9284
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-25 15:54:14 -0400

    Add button to RecipeBrief component
    The button passes the id of the selected recipe to the function handleRecipeBriefClick.

--- src/components/RecipesDisplay/RecipeBrief.jsx
+++ src/components/RecipesDisplay/RecipeBrief.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 
 export default function RecipesBrief({ recipes }) {
+  function handleRecipeBriefClick(id) {
+    console.log(`The recipe id is ${id}`);
+  }
+
   const data = recipes && recipes.map((recipe) => {
-    const { name, thumbnail_url: thumbnailUrl } = recipe;
+    const { name, thumbnail_url: thumbnailUrl, id } = recipe;
+
     return (
       <div key={nanoid()}>
         <img src={thumbnailUrl} />
         <h1>{name}</h1>
-        {/* Link for Details */}
+        <button
+          onClick={() => handleRecipeBriefClick(id)}
+        >
+          View recipe details
+        </button>
       </div>
     );
   });
