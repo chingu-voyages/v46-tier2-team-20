a508f7d430a9b4f8f7ce3eeb0c3ea6faaa29a5cb
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-25 16:23:13 -0400

    Changed handleRecipeBriefClick in RecipeBrief.jsx
    The function now passes the whole recipe back to App.jsx, not just the id

--- src/components/recipeBrief/RecipeBrief.jsx
+++ src/components/recipeBrief/RecipeBrief.jsx
@@ -10,7 +10,7 @@ export default function RecipesBrief({ recipes, handleRecipeBriefClick }) {
         <img src={thumbnailUrl} />
         <h1>{name}</h1>
         <button
-          onClick={() => handleRecipeBriefClick(id)}
+          onClick={() => handleRecipeBriefClick(recipe)}
         >
           View recipe details
         </button>
