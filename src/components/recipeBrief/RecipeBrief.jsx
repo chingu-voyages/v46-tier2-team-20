22c21fc66b6d7d5e739a5848b1f9f906423f7423
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-25 16:03:44 -0400

    Update RecipeBrief.jsx
    Removed handleRecipeBrief click function - moved it up to App.jsx

--- src/components/recipeBrief/RecipeBrief.jsx
+++ src/components/recipeBrief/RecipeBrief.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
-export default function RecipesBrief({ recipes }) {
-  function handleRecipeBriefClick(id) {
-    console.log(`The recipe id is ${id}`);
-  }
-
+export default function RecipesBrief({ recipes, handleRecipeBriefClick }) {
   const data = recipes && recipes.map((recipe) => {
     const { name, thumbnail_url: thumbnailUrl, id } = recipe;
 
