a14bc2b3df685d04c82f67956deb3d9e05fed99a
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-26 10:27:00 -0400

    Removed id from the recipe deconstruction in RecipeBrief
    No longer needed

--- src/components/recipeBrief/RecipeBrief.jsx
+++ src/components/recipeBrief/RecipeBrief.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 
 export default function RecipesBrief({ recipes, handleRecipeBriefClick }) {
   const data = recipes && recipes.map((recipe) => {
-    const { name, thumbnail_url: thumbnailUrl, id } = recipe;
+    const { name, thumbnail_url: thumbnailUrl } = recipe;
 
     return (
       <div key={nanoid()}>
