6805adbf34363870f81d20c04867b0d9aadb8e58
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-26 10:27:50 -0400

    Added destructuring of recipeDetail data in SummaryDetail component
    Created ingredientEls and nutritionEls

--- src/components/summaryDetail/SummaryDetail.jsx
+++ src/components/summaryDetail/SummaryDetail.jsx
@@ -1,9 +1,34 @@
-export default function SummaryDetail(props) {
+import { nanoid } from 'nanoid';
+
+export default function SummaryDetail({ recipeDetail }) {
+  const {
+    name, thumbnail_url: thumbnailUrl, video_url: videoUrl, nutrition, instructions, sections, tags,
+  } = recipeDetail;
+
+  const ingredientEls = sections.map((section) => section.components.map((component) => (<p key={nanoid()}>{component.raw_text }</p>)));
+
+  let nutritionEls = null;
+  if (Object.keys(nutrition).length === 0) {
+    return nutritionEls = null;
+  }
+  nutritionEls = Object.entries(nutrition)
+    .filter(([key, value]) => key !== 'updated_at')
+    .map(([key, value]) => (
+      <p key={key}>
+        {key}
+        {': '}
+        {value}
+      </p>
+    ));
+
   return (
-    <h1>
-      I want to cook
-      {' '}
-      {props.recipeDetail.name}
-    </h1>
+    <>
+      <h2>{name}</h2>
+      <h3>Ingredients</h3>
+      {ingredientEls}
+      <h3>Nutrition</h3>
+      {nutritionEls}
+      <h3>Instructions</h3>
+    </>
   );
 }
