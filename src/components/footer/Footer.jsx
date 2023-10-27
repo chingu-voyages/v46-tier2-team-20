f427cd1e641e2e07e72e85ce5a900bf56f7547c0
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-10-27 14:53:28 -0400

    Reorganized the designer and developer display
    Realigned to match design. Now there is a gap between the titles and the names, and the start of the names are aligned

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,35 +8,37 @@ function Footer() {
         <p>
           © 2023 All rights reserved
         </p>
-        <div className="dev-info">
-          <p>
-            Designer:
-            {' '}
+        <div className="dev-info flex gap-4">
+          <div>
+            <p>Designer:</p>
+            <p>Developers:</p>
+          </div>
+          <div>
             <span>
               <a href="https://github.com/nghia-nguyen-dev" target="blank">
                 Nghia
               </a>
             </span>
-          </p>
-          <p>
-            Developers:
-            {' '}
-            <span>
-              <a href="https://github.com/AOA19">Alexandra</a>
-            </span>
-            {' '}
-            <span>
-              <a href="https://github.com/allison-truhlar" target="blank">Allison</a>
-            </span>
-            {' '}
-            <span>
-              <a href="https://github.com/Emimint" target="blank">Emimint</a>
-            </span>
-            {' '}
-            <span>
-              <a href="https://github.com/komalgill0310" target="blank">Komal</a>
-            </span>
-          </p>
+            <div>
+              <span>
+                <a href="https://github.com/AOA19">Alexandra</a>
+              </span>
+              {' '}
+              <span>
+                <a href="https://github.com/allison-truhlar" target="blank">Allison</a>
+              </span>
+              {' '}
+              <span>
+                <a href="https://github.com/Emimint" target="blank">Emimint</a>
+              </span>
+              {' '}
+              <span>
+                <a href="https://github.com/komalgill0310" target="blank">Komal</a>
+              </span>
+            </div>
+
+          </div>
+
         </div>
         <div className="git-logo">
           <a href="https://github.com/chingu-voyages/v46-tier2-team-20" target="blank"><img src="/github-mark-white.png" alt="github-logo" width="30" height="30" /></a>
