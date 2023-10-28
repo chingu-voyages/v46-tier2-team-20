3c38661ea34c49143c4fe6c8231e6dd20651f154
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-28 18:28:25 -0400

    added class name for recipe card items

--- src/components/RecipesDisplay/RecipeBrief.jsx
+++ src/components/RecipesDisplay/RecipeBrief.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
 
+import './RecipeBrief.css';
+
 export default function RecipesBrief({ recipes }) {
   const data = recipes && recipes.map((recipe) => {
     const { name, thumbnail_url: thumbnailUrl } = recipe;
     return (
-      <div key={nanoid()}>
-        <img src={thumbnailUrl} />
-        <h1>{name}</h1>
+      <div key={nanoid()} className="recipe-preview-container">
+        <img src={thumbnailUrl} className="recipe-image" />
+        <h1 className="recipe-title">{name}</h1>
         {/* Link for Details */}
       </div>
     );
