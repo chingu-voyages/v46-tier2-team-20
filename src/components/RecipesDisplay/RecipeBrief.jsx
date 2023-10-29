29292a572951f3756a4e62a64f63ad59cb755af6
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-29 18:31:28 -0400

    added flex css style to jsx as a tailwind class for recipe card

--- src/components/RecipesDisplay/RecipeBrief.jsx
+++ src/components/RecipesDisplay/RecipeBrief.jsx
@@ -7,7 +7,7 @@ export default function RecipesBrief({ recipes }) {
   const data = recipes && recipes.map((recipe) => {
     const { name, thumbnail_url: thumbnailUrl } = recipe;
     return (
-      <div key={nanoid()} className="recipe-card bg-no-repeat bg-cover bg-center rounded-lg" style={{ backgroundImage: `url(${thumbnailUrl})` }}>
+      <div key={nanoid()} className="recipe-card bg-no-repeat bg-cover bg-center rounded-lg flex flex-col items-start justify-end" style={{ backgroundImage: `url(${thumbnailUrl})` }}>
         <h1 className="recipe-name">{name}</h1>
       </div>
     );
