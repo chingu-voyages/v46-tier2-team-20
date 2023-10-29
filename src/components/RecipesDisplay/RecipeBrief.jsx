dfed35a7cda144d8d6e58598e4de4656d2294e5e
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-29 18:13:58 -0400

    convert px value to relative units

--- src/components/RecipesDisplay/RecipeBrief.jsx
+++ src/components/RecipesDisplay/RecipeBrief.jsx
@@ -7,10 +7,15 @@ export default function RecipesBrief({ recipes }) {
   const data = recipes && recipes.map((recipe) => {
     const { name, thumbnail_url: thumbnailUrl } = recipe;
     return (
-      <div key={nanoid()} className="recipe-preview-container bg-no-repeat bg-cover bg-center rounded-lg" style={{ backgroundImage: `url(${thumbnailUrl})` }}>
+      <div key={nanoid()} className="recipe-card bg-no-repeat bg-cover bg-center rounded-lg" style={{ backgroundImage: `url(${thumbnailUrl})` }}>
         <h1 className="recipe-name">{name}</h1>
       </div>
     );
   });
-  return data;
+  return (
+    <div className="recipe-brief-container">
+      {data}
+    </div>
+
+  );
 }
