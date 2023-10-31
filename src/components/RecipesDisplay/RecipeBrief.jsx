b803d0acdaabb614287baa7bdfc8e53326901dc9
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-31 06:31:57 -0400

    1. Created recipe image as a child of the recipe card 2. Gave class name to the recipe img 3. Deleted background img style from the recipe card

--- src/components/RecipesDisplay/RecipeBrief.jsx
+++ src/components/RecipesDisplay/RecipeBrief.jsx
@@ -7,13 +7,14 @@ export default function RecipesBrief({ recipes }) {
   const data = recipes && recipes.map((recipe) => {
     const { name, thumbnail_url: thumbnailUrl } = recipe;
     return (
-      <div key={nanoid()} className="recipe-card bg-no-repeat bg-cover bg-center rounded-lg flex flex-col items-start justify-end" style={{ backgroundImage: `url(${thumbnailUrl})` }}>
+      <div key={nanoid()} className="recipe-card">
         <h1 className="recipe-name">{name}</h1>
+        <img src={thumbnailUrl} alt="" className="recipe-img" />
       </div>
     );
   });
   return (
-    <div className="recipe-brief-container">
+    <div className="recipe-brief-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-4 gap-y-4">
       {data}
     </div>
 
