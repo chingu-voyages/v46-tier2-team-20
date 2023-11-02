58d3779f838e39aff6fc5e5cfde78e2dd7c522f3
AOA19 <aakinyemi19@gmail.com>
2023-11-01 22:40:11 -0400

    Added validation to search bar, moved ingredient state and search function from App.jsx to SearchBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,15 @@ import SearchBar from './components/searchBar/SearchBar';
 
 function App() {
   // I'm thinking setIngredients will be passed as props to the search bar
-  const [ingredients, setIngredients] = useState('');
   const [recipes, setRecipes] = useState(null);
-  const [isSearching, setIsSearching] = useState(false);
+  // const [isSearching, setIsSearching] = useState(false);
   const [error, setError] = useState('Add an ingredient to search!');
 
   // This function could potentially be moved out to a "utils" folder?
   // In that case, I think the response or error could be returned,
   // and then used with setRecipes or setError here
   async function fetchData(ingredientString) {
-    setIsSearching(true);
+    // setIsSearching(true);
 
     const options = {
       method: 'GET',
@@ -45,43 +44,19 @@ function App() {
         // This error message should be replaced with the error component
         setError('No recipes found - try a different ingredient');
       }
-      setIsSearching(false);
+      // setIsSearching(false);
     } catch (error) {
       setError(error);
-      setIsSearching(false);
+      // setIsSearching(false);
     }
-  }
-
-  // I'm thinking this function will be passed as props to the search bar
-  // I'm thinking this function will be passed as props to the search bar
-  function handleSearch(e) {
-    e.preventDefault();
-
-    if (ingredients.length === 0) {
-      setError('Please input an ingredient(s)');
-      // return;
-    } else if (ingredients.length < 2) {
-      setError('Input must be a least 2 characters');
-    }
-
-    fetchData(ingredients);
   }
 
   return (
     <div>
       <Header />
       <SearchBar
-        handleSearch={handleSearch}
-        setIngredients={setIngredients}
+        fetchData={fetchData}
       />
-      {isSearching && <p>Searching...</p>}
-      {recipes ? (
-        <p>
-          {recipes.length}
-          {' '}
-          recipes found!
-        </p>
-      ) : <p>{error}</p>}
       <RecipeBrief recipes={recipes} />
     </div>
   );
