997c98971aaf211d9e518c3dc124781e917bcad0
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-11-07 13:27:47 -0500

    Changed backgroundBlur to BackgroundBlur
    Functions should have a capital letter for the first letter

--- src/components/backgroundBlur/BackgroundBlur.jsx
+++ src/components/backgroundBlur/BackgroundBlur.jsx
@@ -1,6 +1,6 @@
 import "./BackgroundBlur.css"
 
-export default function backgroundBlur({handleSummaryDetailClose}) {
+export default function BackgroundBlur({handleSummaryDetailClose}) {
   return (
     <div className="bg-blur absolute top-0 left-0 h-full w-full" onClick={handleSummaryDetailClose}>
     </div>
