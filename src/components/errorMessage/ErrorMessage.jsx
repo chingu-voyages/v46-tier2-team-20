9d719b277215874833290b4a95c693edabe18331
Emimint <90863470+Emimint@users.noreply.github.com>
2023-11-07 17:16:34 -0500

    update ErrorMessage design and elements

--- src/components/errorMessage/ErrorMessage.jsx
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import './ErrorMessage.css';
 
 function ErrorMessage({ message }) {
   return (
-    <h1 className="h-screen font-semibold text-3xl">
-      {message}
-      {' '}
-    </h1>
+    <div className="error-container">
+      <img src="./public/researching.png" alt="error" />
+      <h1 className="font-semibold">
+        {message}
+        {' '}
+      </h1>
+    </div>
   );
 }
 
