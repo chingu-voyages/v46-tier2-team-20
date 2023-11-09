da96946c65661ac65f8774586ad24f082c329a6f
Emimint <90863470+Emimint@users.noreply.github.com>
2023-11-09 11:51:38 -0500

    update StatusMessage

--- src/components/statusMessage/StatusMessage.jsx
+++ src/components/statusMessage/StatusMessage.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './StatusMessage.css';
 
-function StatusMessage({ message, errorCode }) {
+function StatusMessage({ message, statusCode }) {
   let imageurl = './public/';
 
-  switch ((`${errorCode}`)[0]) {
+  switch ((`${statusCode}`)[0]) {
     case '2': // Synthax error
       imageurl += 'researching.png';
       break;
     case '4': // Client side error
       imageurl += 'error_404.png';
       break;
-    case '5': // Server side error
-      imageurl = 'technical_error.png';
-      break;
     default: // any other error
-      imageurl = 'no_results.png';
+      imageurl = 'technical_error.png';
   }
 
   return (
@@ -23,7 +20,6 @@ function StatusMessage({ message, errorCode }) {
       <img src={imageurl} alt={imageurl} />
       <h1 className="font-semibold">
         {message}
-        {' '}
       </h1>
     </div>
   );
