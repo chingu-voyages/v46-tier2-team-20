d6a26f08a5bc30e85be5f7ccc2f644f752a6de79
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-11-10 08:03:46 -0500

    1. Change the css and html elements to have the image takes the entire screen width without hiding the image content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,24 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="sm:px-25 header text-center h-screen place-items-center">
-      {/* <img src="/header_img.jpg" alt="" /> */}
-      <h1 className="font-semibold">What will you make today?</h1>
-      <h2 className="">
-        App name is an app that helps you find and create delicious dishes base on your ingredients!
-      </h2>
+    <div className="header">
+      <img src="/header_img.jpg" alt="" />
+      <div className="text-center place-items-center">
+        <h1 className="font-semibold">What will you make today?</h1>
+        <h2 className="">
+          App name is an app that helps you find and create delicious dishes base on your ingredients!
+        </h2>
+      </div>
     </div>
   );
 }
 
 export default Header;
+
+{ /* <div className="sm:px-25 header text-center h-screen place-items-center">
+<img src="/header_img.jpg" alt="" />
+<h1 className="font-semibold">What will you make today?</h1>
+<h2 className="">
+  App name is an app that helps you find and create delicious dishes base on your ingredients!
+</h2>
+</div> */ }
