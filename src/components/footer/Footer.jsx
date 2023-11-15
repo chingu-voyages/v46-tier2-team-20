c7c458ed3d32a890c39300c1d536a001f5cfa684
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-11-15 13:44:19 -0500

    Changed the footer from "fixed" to "sticky"
    This way, when the page becomes populated with recipes, the footer gets pushed to the very bottom of the recipes, instead of always being at the bottom of the viewport

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footer fixed bottom-0 left-0">
+    <div className="footer sticky top-0 left-0">
 
       <div className="footer-container">
 
@@ -14,7 +14,7 @@ function Footer() {
         <div className="flex items-center">
 
           <div className="pr-2">
-            <a href="https://github.com/chingu-voyages/v46-tier2-team-20" target="_blank"><img src="/github-mark-white.png" alt="github-logo" width="30" height="30" /></a>
+            <a href="https://github.com/chingu-voyages/v46-tier2-team-20" target="_blank" rel="noreferrer"><img src="/github-mark-white.png" alt="github-logo" width="30" height="30" /></a>
           </div>
 
           <div>
@@ -24,17 +24,17 @@ function Footer() {
 
           <div>
             <div>
-              <a href="https://github.com/AOA19" target="_blank">AOA19</a>
+              <a href="https://github.com/AOA19" target="_blank" rel="noreferrer">AOA19</a>
               {' '}
-              <a href="https://github.com/allison-truhlar" target="_blank">Allison</a>
+              <a href="https://github.com/allison-truhlar" target="_blank" rel="noreferrer">Allison</a>
               {' '}
-              <a href="https://github.com/Emimint" target="_blank">Emimint</a>
+              <a href="https://github.com/Emimint" target="_blank" rel="noreferrer">Emimint</a>
               {' '}
-              <a href="https://github.com/komalgill0310" target="_blank">Komal</a>
+              <a href="https://github.com/komalgill0310" target="_blank" rel="noreferrer">Komal</a>
             </div>
 
             <div className="flex">
-              <a href="https://github.com/nghia-nguyen-dev" target="_blank">
+              <a href="https://github.com/nghia-nguyen-dev" target="_blank" rel="noreferrer">
                 Nghia
               </a>
             </div>
