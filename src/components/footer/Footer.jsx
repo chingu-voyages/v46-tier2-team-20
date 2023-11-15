4b768b40d909df54ed8ab69ef51200bbbeca2d8a
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-11-15 12:03:12 -0500

    Changes to Footer.jsx and Footer.css
    These changes fixed an issue where, at intermediate screen sizes, the developer and designer names went into columns that made it difficult to read.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,45 +3,44 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footer">
+    <div className="footer fixed bottom-0 left-0">
+
       <div className="footer-container">
+
         <p>
           © 2023 All rights reserved
         </p>
-        <div className="flex gap-2">
-          <div>
-            <p>Designer:</p>
-            <p>Developers:</p>
+
+        <div className="flex items-center">
+
+          <div className="pr-2">
+            <a href="https://github.com/chingu-voyages/v46-tier2-team-20" target="blank"><img src="/github-mark-white.png" alt="github-logo" width="30" height="30" /></a>
           </div>
+
           <div>
-            <span>
+            <p>Developers:</p>
+            <p>Designer:</p>
+          </div>
+
+          <div>
+            <div>
+              <a href="https://github.com/AOA19">Alexandra</a>
+              {' '}
+              <a href="https://github.com/allison-truhlar" target="blank">Allison</a>
+              {' '}
+              <a href="https://github.com/Emimint" target="blank">Emimint</a>
+              {' '}
+              <a href="https://github.com/komalgill0310" target="blank">Komal</a>
+            </div>
+
+            <div className="flex">
               <a href="https://github.com/nghia-nguyen-dev" target="blank">
                 Nghia
               </a>
-            </span>
-            <div>
-              <span>
-                <a href="https://github.com/AOA19">Alexandra</a>
-              </span>
-              {' '}
-              <span>
-                <a href="https://github.com/allison-truhlar" target="blank">Allison</a>
-              </span>
-              {' '}
-              <span>
-                <a href="https://github.com/Emimint" target="blank">Emimint</a>
-              </span>
-              {' '}
-              <span>
-                <a href="https://github.com/komalgill0310" target="blank">Komal</a>
-              </span>
             </div>
 
           </div>
 
-        </div>
-        <div className="git-logo">
-          <a href="https://github.com/chingu-voyages/v46-tier2-team-20" target="blank"><img src="/github-mark-white.png" alt="github-logo" width="30" height="30" /></a>
         </div>
       </div>
     </div>
