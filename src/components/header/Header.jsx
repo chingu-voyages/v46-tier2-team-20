d56625e5aab0c77b65e2a980158b4fd271826f83
AOA19 <aakinyemi19@gmail.com>
2023-11-17 18:27:37 -0500

    Fixed header styling bug, readded logo and favicon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import './Header.css';
 
 function Header() {
   return (
-    <div className="sm:px-25 header text-center h-screen place-items-center">
+    <div className="sm:px-25 header text-center h-screen place-items-center relative">
+      <img src="/logo.svg" alt="Logo" className="logo absolute" />
       <h1 className="font-semibold">What will you make today?</h1>
-      <h2 className="">
-        App name is an app that helps you find and create delicious dishes base on your ingredients!
+      <h2>
+        FlavrFindr is an app that helps you find and create
+        delicious dishes base on your ingredients!
       </h2>
     </div>
   );
