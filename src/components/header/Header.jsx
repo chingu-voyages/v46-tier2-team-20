1867d866600c98fa350bf549ec416cf9a789f18c
AOA19 <aakinyemi19@gmail.com>
2023-11-17 18:44:39 -0500

    Added padding to Header div to create more space between the logo and header text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="sm:px-25 header text-center h-screen place-items-center relative">
+    <div className="sm:px-25 header text-center h-screen place-items-center relative pt-8">
       <img src="/logo.svg" alt="Logo" className="logo absolute" />
       <h1 className="font-semibold">What will you make today?</h1>
       <h2>
