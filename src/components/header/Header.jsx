53d0ff2e42761e3135e61a62ff254590f3b93e9f
AOA19 <aakinyemi19@gmail.com>
2023-11-17 11:19:20 -0500

    Removed comments

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,32 +4,14 @@ import './Header.css';
 function Header() {
   return (
     <div className="sm:px-25 header text-center h-screen place-items-center relative">
-      {/* <img src="/header_img.jpg" alt="" /> */}
-      <img src="/logo.png" alt="logo" className="absolute logo" />
+      <img src="/logo.svg" alt="logo" className="absolute logo" />
       <h1 className="font-semibold">What will you make today?</h1>
       <h2>
-        FlavrFindr is an app that helps you find and create delicious dishes base on your ingredients!
+        FlavrFindr is an app that helps you find and create
+        delicious dishes base on your ingredients!
       </h2>
     </div>
   );
 }
 
 export default Header;
-
-{ /* <div className="sm:px-25 header text-center h-screen place-items-center">
-<img src="/header_img.jpg" alt="" />
-<h1 className="font-semibold">What will you make today?</h1>
-<h2 className="">
-  App name is an app that helps you find and create delicious dishes base on your ingredients!
-</h2>
-</div> */ }
-
-{ /* <div className="header">
-<img src="/header_img.jpg" alt="" />
-<div className="text-center place-items-center">
-  <h1 className="font-semibold">What will you make today?</h1>
-  <h2 className="">
-    App name is an app that helps you find and create delicious dishes base on your ingredients!
-  </h2>
-</div>
-</div> */ }
